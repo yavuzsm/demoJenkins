050682425ad48bca4eca02bd76a8ff4ddf7f4b00
Yavuz Selim <yavuzsm44@gmail.com>
2024-05-01 17:45:10 +0300

    update

--- src/main/java/com/example/demo/controller.java
+++ src/main/java/com/example/demo/controller.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class controller {
     @GetMapping
     public String deneme1(){
-        return "denem124124";
+        return "asdasdasdasdas";
     }
 }
