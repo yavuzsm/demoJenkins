4c1ab6e8738aadea6176d55f6ed3160b5d5a3e25
Yavuz Selim <yavuzsm44@gmail.com>
2024-05-02 15:41:32 +0300

    update

--- src/main/java/com/example/demo/controller.java
+++ src/main/java/com/example/demo/controller.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class controller {
     @GetMapping
     public String deneme1(){
-        return "asdasd";
+        return "yavuz selim mızıkacı";
     }
 }
